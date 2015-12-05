99a3fbd246c56eb86935a087eaa81681b857d591
Bill Ho <Bill_ho@foxmail.com>
2015-12-05 17:47:07 +0800

    添加 Lombok 支持，简化代码。

--- src/main/java/ext/billopen/start/db/entity/StartUpDemo.java
+++ src/main/java/ext/billopen/start/db/entity/StartUpDemo.java
@@ -6,31 +6,19 @@ import javax.persistence.Id;
 
 import org.hibernate.annotations.GenericGenerator;
 
+import lombok.Data;
+
 /**
 * @author  Bill_Ho@foxmail.com
 * @version 2015年12月5日 下午3:39:42
 * 
 */
 @Entity
+@Data
 public class StartUpDemo {
 	@GenericGenerator(name = "generator", strategy = "uuid.hex")  
     @Id  
     @GeneratedValue(generator = "generator")
 	private String id;
-	
 	private String content;
-	
-	public String getId(){
-		return id;
-	}
-	public void setId(String id){
-		this.id= id;
-	}
-	
-	public String getContent(){
-		return content;
-	}
-	public void setContent(String content){
-		this.content= content;
-	}
 }
